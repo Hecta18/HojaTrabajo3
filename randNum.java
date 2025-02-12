ac2ff4bdf7846be3c8fbc2ce97d507e007f1f1a7
Hecta18 <62799494+Hecta18@users.noreply.github.com>
2025-02-11 21:25:49 -0600

    creacion de .txt con numeros aleatorios

--- randNum.java
+++ randNum.java
@@ -1,16 +1,38 @@
-// El programa genera un archivo con un numero aleatorio
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class randNum {
-
-    public static void main(String[] args) {
-        int randNum = (int) (Math.random() * 100);//numero de 0 a 100
-        try(FileWriter fileWriter = new FileWriter("randNum.txt");
-        PrintWriter printWriter = new PrintWriter(fileWriter);) {
-            printWriter.println(randNum);
-        } catch (Exception e) {
+    // ingresar cantidad de numeros entre 10 y 3000
+    public int getNum() {
+        Scanner scanner = new Scanner(System.in);
+            System.out.println("Ingrese la cantidad de numeros a generar: ");
+            int num = scanner.nextInt();
+            scanner.close();
+            if (num < 10 || num > 3000) {
+                System.out.println("Error: Ingrese un numero entre 10 y 3000");
+                getNum();
+            } else {
+                return num;  
+            }
+        }
+    }
+    public void printNum() {
+        int cantNums = getNum();
+        try (FileWriter fileWriter = new FileWriter("randNum.txt");
+                PrintWriter printWriter = new PrintWriter(fileWriter)) {
+            for (int i = 0; i < cantNums; i++) {
+                int ranNum = (int) (Math.random() * 100);// numero de 0 a 100
+                printWriter.println(ranNum);
+            }
+        } catch (IOException e) {
             System.out.println("Error: " + e);
         }
+    }
+
+    public static void main(String[] args) {
+        randNum generator = new randNum();
+        generator.printNum();
     }
 }
